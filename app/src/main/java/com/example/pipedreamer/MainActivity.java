58478d9f7550800e05acb765650ed3b15d1735a1
Bernardo Falé <mbfale@ua.pt>
2022-05-31 10:50:46 +0100

    new version

--- app/src/main/java/com/example/pipedreamer/MainActivity.java
+++ app/src/main/java/com/example/pipedreamer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.pipedreamer.databinding.ActivityMainBinding;
@@ -19,29 +20,21 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
-        replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch (item.getItemId()){
                 case R.id.home:
-                    replaceFragment(new HomeFragment());
+
                     break;
                 case R.id.calendar:
-                    replaceFragment(new CalendarFragment());
+                    Intent intent = new Intent(this, CalendarActivity.class);
                     break;
                 case R.id.list:
-                    replaceFragment(new ListFragment());
+
                     break;
             }
             return true;
         });
-    }
-
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
     }
 }
